001c67674534b3e5eda0241bb3945fd5c96255e3
amirulasri <amirulasrix@gmail.com>
2023-08-08 02:25:54 +0800

    Initial commit

---
+++ src/businessfilereader/BusinessFileReader.java
@@ -0,0 +1,20 @@
+package businessfilereader;
+
+import FrameUI.Home;
+import java.io.IOException;
+
+/**
+ *
+ * @author Amirul Asri
+ */
+public class BusinessFileReader {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        Home homeframe = new Home();
+        homeframe.setVisible(true);
+    }
+
+}
